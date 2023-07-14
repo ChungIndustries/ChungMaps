7c7fb646ddb5007fc8fa16621a1ed968f9b93973
Christian <chrille_0313@hotmail.com>
2023-07-14 04:20:18 +0200

    Update page.tsx

--- chung_maps_site/src/app/world/page.tsx
+++ chung_maps_site/src/app/world/page.tsx
@@ -1,4 +1,16 @@
 
+import PocketBase from 'pocketbase';
+// import eventsource from 'eventsource';
+
+// global.EventSource = eventsource as any;
+
+// const pb = new PocketBase('http://127.0.0.1:8090');
+
+// pb.collection('blocks').subscribe('*', (e) => {
+//     console.log(e.action);
+//     console.log(e.record);
+// });
+
 
 export default function World() {
     return (
