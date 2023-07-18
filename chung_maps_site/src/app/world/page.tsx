7753176c2b69547d50d273ae874a1ebda0039c17
Christian <chrille_0313@hotmail.com>
2023-07-18 02:29:56 +0200

    remove revalidation for safety measure

--- chung_maps_site/src/app/world/page.tsx
+++ chung_maps_site/src/app/world/page.tsx
@@ -1,8 +1,10 @@
-
 import WorldViewer from './components/world_viewer';
 
+// FIXME: Not sure if this is needed
+export const revalidate = 0;
 
-export default function World() {
+
+export default async function World() {
     return (
         <WorldViewer />
     );
