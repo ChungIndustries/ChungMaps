ff6674f0226085c1c0c0f1e5aa6ea43c321ea35c
Christian <47303717+chrille0313@users.noreply.github.com>
2023-07-18 06:47:15 +0200

    Delete ChungMapsSite directory
    No longer using Django
    
    Signed-off-by: Christian <47303717+chrille0313@users.noreply.github.com>

--- ChungMapsSite/API/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import MapExplorerKey
-from rest_framework_api_key.admin import APIKeyModelAdmin
-
-
-@admin.register(MapExplorerKey)
-class MapExplorerKeyAdmin(APIKeyModelAdmin):
-    pass
--- ChungMapsSite/MapViewer/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import MinecraftBlock, MapExplorer
-
-
-admin.site.register(MinecraftBlock)
-admin.site.register(MapExplorer)
