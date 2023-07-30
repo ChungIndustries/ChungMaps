e2e6f5433ab34c6e23b76aa2641813f96f4e53d3
Christian <chrille_0313@hotmail.com>
2023-07-30 16:05:44 +0200

    Add POST to block subroutes

--- chung_maps_site/src/app/api/blocks/[dimension]/[x]/[y]/[z]/route.tsx
+++ chung_maps_site/src/app/api/blocks/[dimension]/[x]/[y]/[z]/route.tsx
@@ -17,3 +17,24 @@ export async function GET(request: Request, context: { params: { dimension: stri
         });
     });
 }
+
+
+export async function POST(request: Request, context: { params: { dimension: string, x: number, y: number, z: number } }) {
+    return getResponse(async () => {
+        const { x, y, z, dimension } = context.params;
+        const db = getDB();
+        const data = await request.json();
+        data.x = x;
+        data.y = y;
+        data.z = z;
+        data.dimension = dimension;
+
+        const { username, api_key } = data;
+
+        await db.collection('map_explorers').authWithPassword(username, api_key);
+        const record = await db.collection('blocks').create<MinecraftBlock>(data);
+        db.authStore.clear();
+
+        return record;
+    })
+}
